49951943dbc58467290726d88eca748b29c5f7a5
Eun Ju Jhong <eun01300714@gmail.com>
2018-11-29 19:18:11 +0900

    make an onCreate Log

--- app/src/main/java/kr/emirim/eun01/l03_glide/MainActivity.java
+++ app/src/main/java/kr/emirim/eun01/l03_glide/MainActivity.java
@@ -2,12 +2,15 @@ package kr.emirim.eun01.l03_glide;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    final static String TAG = "Main Activity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.e(TAG, "onCreate!");
     }
 }
